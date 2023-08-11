7d3113ef1ff9c0ab9e97e031f4c15522ad99798d
carloseoz <137317522+carloseoz@users.noreply.github.com>
2023-08-11 11:13:40 +0100

    Create README.m

---
+++ README.m
@@ -0,0 +1,7 @@
+- 👋 Hi, I’m @carloseoz
+- 👀 I’m interested in crypto
+- 🌱 I’m currently learning ordinals and BRC20
+- 💞️ I’m looking to collaborate on crypto projects
+- 📫 How to reach me @Carloseoz2 twiter
+
+
